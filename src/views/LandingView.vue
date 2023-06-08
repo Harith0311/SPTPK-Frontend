6e95f3c019c3ea4ba4df785000f2965c2f2b8c58
Harith <harithismail29@gmail.com>
2023-06-09 00:33:48 +0800

    update

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -5,24 +5,27 @@ import LightBlueButton from '../components/LightBlueButton.vue';
 </script>
 
 <template >
-    <div class="relative w-screen max-md:bg-slate-400 max-md:h-screen">
-        <img src="../assets/First-pageee.png" alt="" class="absolute w-screen h-screen -z-10 max-md:hidden">
+    <div class="relative bg-gradient-to-r from-cyan-300 to-blue-700 w-screen h-screen max-md:bg-blue-200 max-md:h-screen">
+        <!-- <div class="invisible max-md:visible max-md:flex max-md:justify-center" >
+            <img src="../assets/permata-logo.png" alt="" class="m-5 h-auto w-12">
+            <img src="../assets/ypk-logo.png" alt="" class="m-5 h-auto w-16">
+        </div> -->
         <div class="landing text-2xl text-center text-black hover:text-red-600 duration-700 ">
             <h1 class="pt-28 max-md:pt-10 font-bold">SISTEM PENGURUSAN TASKA PERMATA KELUARGA</h1>
         </div>
-        <div class="p-4 px-16 mx-auto mt-5 max-w-2xl block bg-white h-auto rounded-lg justify-center ">
-            <div class="flex justify-center gap-12 ">
+        <div class="p-4 px-16 mx-auto mt-5 max-w-2xl block bg-blue-300 bg-opacity-70 h-auto rounded-lg justify-center ">
+            <div class="flex justify-center gap-12 opacity-100">
                 <img src="../assets/permata-logo.png" alt="" class="max-md:hidden h-auto w-16">
                 <img src="../assets/ypk-logo.png" alt="" class="max-md:hidden h-auto w-20">
             </div>
-                <ul class="m-5 text-center font-semibold">
-                    <BlueButton><RouterLink to="/register">Daftar Masuk</RouterLink></BlueButton>
+                <ul class="bg-blue-400 opacity-100 rounded-md p-2 px-20 drop-shadow-lg hover:scale-[1.02] transform transition duration-300 ease-in-out m-5 text-center font-semibold ">
+                    <button><RouterLink to="/register">Daftar Masuk</RouterLink></button>
                 </ul>
-                <ul class="m-5 text-center font-semibold">
-                    <BlueButton><RouterLink to="/login">Log Masuk</RouterLink></BlueButton>
+                <ul class="bg-blue-400 opacity-100 rounded-md p-2 px-20 drop-shadow-lg hover:scale-[1.02] transform transition duration-300 ease-in-out m-5 text-center font-semibold">
+                    <button><RouterLink to="/login">Log Masuk</RouterLink></button>
                 </ul>
-                <ul class="m-5 text-center font-semibold">
-                    <LightBlueButton><RouterLink to="/registrationForm1">Pendaftaran Baharu</RouterLink></LightBlueButton>
+                <ul class="bg-blue-300 opacity-100 rounded-md p-2 px-20 drop-shadow-lg hover:scale-[1.02] transform transition duration-300 ease-in-out m-5 text-center font-semibold">
+                    <button><RouterLink to="/registrationForm1">Pendaftaran Baharu</RouterLink></button>
                 </ul>       
                       
         </div>
