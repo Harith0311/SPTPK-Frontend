57498c1ff5621ea97d67b494164607e83b911301
Harith <harithismail29@gmail.com>
2023-06-12 02:27:22 +0800

    update

--- src/components/ToastMessage.vue
+++ src/components/ToastMessage.vue
@@ -1,36 +1,40 @@
-<template>
+<template >
+    <template class="flex justify-center">
     <transition name="toast">
         <!-- v-if="isVisible" -->
-     <div  class="fixed top-10 right-4 z-50"> 
-         <div :class="toastClasses" class="w-64">
-              <div class="flex items-center">
-           <span class="mr-2">
-             <!-- Add appropriate icon based on the status -->
-             <i v-if="type === 'success'" class="fa-solid fa-circle-check text-3xl text-green-700"></i>
-             <i v-else-if="type === 'error'" class="fa-solid fa-circle-xmark text-3xl text-red-500"></i>
-             <i v-else class="fa-solid fa-circle-check text-3xl"></i>
-           </span>
-           <div class="ml-2">
-             <!-- Display the status text -->
-             <div class="text-sm font-medium">{{ message.status }}</div>
-             <!-- Display the message text -->
-             <div class="mb-1 font-light">{{ message.text }}</div>
-           </div>
-         </div>
-         </div>
-     </div>
-     </transition>
+    
+        <div v-if="isVisible" class="fixed top-6  z-50"> 
+            <div :class="toastClasses" class="w-64">
+                 <div class="flex items-center">
+              <span class="mr-2">
+                <!-- Add appropriate icon based on the status -->
+                <i v-if="type === 'success'" class="fa-solid fa-circle-check text-3xl Z text-black"></i>
+                <i v-else-if="type === 'error'" class="fa-solid fa-circle-xmark text-3xl text-red-500"></i>
+                <i v-else class="fa-solid fa-circle-check text-3xl"></i>
+              </span>
+              <div class="ml-2">
+                <!-- Display the status text -->
+                <div class="text-sm font-medium">{{ message.status }}</div>
+                <!-- Display the message text -->
+                <div class="mb-1 font-light">{{ message.text }}</div>
+              </div>
+            </div>
+            </div>
+        </div>
+    
+     </transition>]
+    </template>
  </template>
  
  <style>
     /* enter transitions */
     .toast-enter-from {
      opacity: 0;
-     transform: translateX(100%);
+     transform: translateY(-100%);
    }
    .toast-enter-to {
      opacity: 1;
-     transform: translateX(0);
+     transform: translateY(0);
    }
    .toast-enter-active {
      transition: all 0.3s ease;
@@ -39,11 +43,11 @@
    /* leave transitions */
    .toast-leave-from {
      opacity: 1;
-     transform: translateX(0);
+     transform: translateY(0);
    }
    .toast-leave-to {
      opacity: 0;
-     transform: translateX(100%);
+     transform: translateY(-100%);
    }
    .toast-leave-active {
      transition: all 0.3s ease;
@@ -71,7 +75,7 @@
                  return[
                      'px-4 py-4 rounded-lg  text-sm font-medium drop-shadow-2xl',
                      {
-                         'bg-[#c5f7dd]': this.type==='success',
+                         'bg-[#69d67b]': this.type==='success',
                          'bg-[#ffd0cb]': this.type === 'error',
                          'bg-green-400 ': this.type === 'info' 
                      },
