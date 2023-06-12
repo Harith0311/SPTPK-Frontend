35fc54a6b1250d9c63b2b960f767f281502a9d60
Harith <harithismail29@gmail.com>
2023-06-13 03:04:33 +0800

    update

--- src/views/ReportBaseView.vue
+++ src/views/ReportBaseView.vue
@@ -26,11 +26,11 @@
             <div class="bg-green-200  h-[480px] w-full mx-auto mt-5 mb-10 rounded-2xl pb-10">
                 <h2 class="text-xl font-bold px-14 p-5">Maklumat Kanak-Kanak</h2>
                 <div class=" relative overflow-y-auto overflow-hidden  h-[380px]">
-                    <table class="m-8 w-5/6 mx-auto relative overflow-y-auto h-[380px] scrollbar-hide">
+                    <table class="m-8 w-5/6 mx-auto relative overflow-y-auto h-[380px] scrollbar-hide" id="childTable">
                         <thead class="sticky top-0 z-10 ">
                             <tr class="border-solid border-b-2 border-black ">
                                 <th class="w-auto bg-green-200">Bil.</th>
-                                <th class="bg-green-200 ">Nama</th>
+                                <th class="">Nama</th>
                                 <th class="bg-green-200">Umur</th>
                                 <th class="bg-green-200">Sijil Lahir</th>
                                 <th class="bg-green-200">Jantina</th>
@@ -52,7 +52,7 @@
 
             
             </div>
-            <div class="bg-green-300 hover:bg-green-400 cursor-pointer p-4 h-[60px] w-full mx-auto mb-10 drop-shadow-lg rounded-2xl text-center font-bold text-lg">
+            <div @click="printReport" class="bg-green-300 hover:bg-green-400 cursor-pointer p-4 h-[60px] w-full mx-auto mb-10 drop-shadow-lg rounded-2xl text-center font-bold text-lg">
                 Cetak Laporan Kanak Kanak
             </div>
 
@@ -62,6 +62,8 @@
 <script>
 
 import axios from 'axios';
+import html2pdf from 'html2pdf.js';
+
 
 export default {
     data() {
@@ -86,7 +88,101 @@ export default {
         },
 
         printReport(){
-            
+
+            // Create a new window for printing
+            const printWindow = window.open('', '_blank', 'width=800,height=600,top=100,left=100,location=no');
+
+            // Open a new document in the print window
+            printWindow.document.open();
+
+            // Extract the data from the Vue.js component
+            const ChildList = this.ChildList;
+
+            // Generate the table rows using the data
+            const tableRows = ChildList.map((child, index) => {
+            return `
+                <tr>
+                <td style="text-align: center;">${index + 1}</td>
+                <td>${child.namaKanak}</td>
+                <td style="text-align: center;">${child.umur}</td>
+                <td style="text-align: center;">${child.sijilLahir}</td>
+                <td style="text-align: center;">${child.jantina}</td>
+                <td style="text-align: center;">${child.bangsa}</td>
+                </tr>
+            `;
+            }).join('');
+
+            // Generate the complete HTML template with the dynamic data
+            const htmlTemplate = `
+            <html>
+                <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    * {
+                    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                    }
+
+                    table {
+                    margin-left: auto;
+                    margin-right: auto;
+                    width: 80vw;
+                    border-collapse: collapse;
+                    }
+
+                    table, td, th {
+                    border: 2px solid black;
+                    padding: 5px;
+                    }
+
+                    h2 {
+                    text-align: center;
+                    margin-top: 40px;
+                    margin-bottom: -5px;
+                    }
+
+                    p {
+                    font-size: 15px;
+                    font-weight: 600;
+                    text-align: center;
+                    margin-bottom: 30px;
+                    }
+                </style>
+                </head>
+                <body>
+                <h2>Laporan Kanak Kanak</h2>
+                <p>TASKA PERMATA KELUARGA TAMAN DESA PERMAI</p>
+                <table>
+                    <thead>
+                    <tr>
+                        <th style="width: 20px;">Bil.</th>
+                        <th>Nama</th>
+                        <th style="width: 70px;">Umur</th>
+                        <th style="width: 140px;">Sijil Lahir</th>
+                        <th style="width: 100px;">Jantina</th>
+                        <th style="width: 100px;">Bangsa</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    ${tableRows}
+                    </tbody>
+                </table>
+                </body>
+            </html>
+            `;
+
+            // Write the HTML template to the print window document
+            printWindow.document.write(htmlTemplate);
+
+            // Close the document after writing
+            printWindow.document.close();
+
+            printWindow.print();
+
+            printWindow.document.querySelector('body').style.margin = '0';
+            printWindow.document.querySelector('table').style.width = '100%';
+            printWindow.document.querySelector('table').style.borderCollapse = 'collapse';
         }
     }
 }
